675a2e7bd0bb703a57516655899fcf565333758b
Gloria-28 <gloriaabera@gmail.com>
2022-06-22 17:35:01 -0400

    Current temperature

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
+
+function displayTemperature(response) {
+    console.log(response.data)
+
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    
+    temperatureElement.innerHTML = Math.round (response.data.main.temp);
+    cityElement.innerHTML = response.data.main.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    
+}
+
 let apiKey=  "11af10924b44b47f1b1d52623ef2ad0b";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New Yorkn&appid=${apiKey}`;
+axios.get(apiUrl). then(displayTemperature);
 
-console.log(apiUrl);
