fb32c0cd04b80473bdef861c8fdf9dc725d3036b
Gloria-28 <gloriaabera@gmail.com>
2022-06-25 17:54:23 -0400

    I added search engine

--- src/app.js
+++ src/app.js
@@ -48,12 +48,27 @@ function displayTemperature(response) {
      iconElement.setAttribute("alt", response.data.weather[0].description);
     
 }
-let apiKey="11af10924b44b47f1b1d52623ef2ad0b";
-let city ="New York";
+
+function search(city){
+    let apiKey="11af10924b44b47f1b1d52623ef2ad0b";
+  
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
  axios.get(apiUrl). then(displayTemperature);
+}
 
-    
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+   
+}
+ 
+
+
+
+    search ("New York");
+
+    let form = document.querySelector("#search-form");
+    form.addEventListener("submit", handleSubmit);
 
 
